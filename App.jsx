673e6fd7cd08d2192382822222e0e1de374e4fed
reef mizan <rym214853137@gmail.com>
2026-06-29 22:48:35 +0300

    added oriantation to lock on portrait mode

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { AuthProvider } from './src/context/AuthContext';
 import { RootNavigator } from './src/navigation/RootNavigator';
 import { ThemeProvider } from './src/context/ThemeContext';
@@ -8,6 +9,14 @@ import { ThemeProvider } from './src/context/ThemeContext';
  * and initializes the Native Routing Controller.
  */
 export default function App() {
+    useEffect(() => {
+        async function lockOrientation() {
+        await ScreenOrientation.lockAsync(
+            ScreenOrientation.OrientationLock.PORTRAIT_UP
+        );
+        }
+        lockOrientation();
+    }, []);
     return (
         <ThemeProvider>
             <AuthProvider>
